8795dff87db95f22823efd2a3b2f5afcbddf1d95
symparosa <symparosa@gmail.com>
2023-07-22 14:24:59 -0100

    relatorio 1.0.0

--- src/main/java/app/api/denuncia/Controllers/AuditoriaController.java
+++ src/main/java/app/api/denuncia/Controllers/AuditoriaController.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
